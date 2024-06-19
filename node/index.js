7ff2495d57dabdbeca53907074f65261b77871c6
diazeddy <eddydiaz0219@gmail.com>
2024-06-19 14:19:29 -0700

    create new api to calculate fiscal responsibility score

--- node/index.js
+++ node/index.js
@@ -625,3 +625,66 @@ app.get('/api/signal_evaluate', function (request, response, next) {
     })
     .catch(next);
 });
+
+app.get('/api/fiscal_responsibility_score', function (request, response, next) {
+  Promise.resolve()
+    .then(async function () {
+      // get account informations
+      const accountsResponse = await client.accountsGet({
+        access_token: ACCESS_TOKEN,
+      });
+
+      // Set cursor to empty to receive all historical updates
+      let cursor = null;
+
+      // New transaction updates since "cursor"
+      let added = [];
+      let modified = [];
+      // Removed transaction ids
+      let removed = [];
+      let hasMore = true;
+      // Iterate through each page of new transaction updates for item
+      while (hasMore) {
+        const request = {
+          access_token: ACCESS_TOKEN,
+          cursor: cursor,
+        };
+        const response = await client.transactionsSync(request)
+        const data = response.data;
+        // Add this page of results
+        added = added.concat(data.added);
+        modified = modified.concat(data.modified);
+        removed = removed.concat(data.removed);
+        hasMore = data.has_more;
+        // Update cursor to the next cursor
+        cursor = data.next_cursor;
+      }
+
+      // sort by dates
+      const compareTxnsByDateAscending = (a, b) => (a.date < b.date) - (a.date > b.date);
+      added.sort(compareTxnsByDateAscending);
+
+      const score = accountsResponse.data.accounts.map(({account_id, balances, name}) => {
+        // filter transaction data by account id
+        const transactions = added.filter((item) => item.account_id === account_id);
+        let balance = balances.current;
+        let count = 0;
+        if (balance > 0) {
+          count++;
+        }
+        transactions.forEach((transaction) => {
+          balance += transaction.amount;
+          if (balance > 0) {
+            count++;
+          }
+        });
+        return {
+          name: name,
+          score: Math.floor(count * 100 / (transactions.length + 1))
+        }
+      })
+
+      response.json(score);
+    })
+    .catch(next);
+})
